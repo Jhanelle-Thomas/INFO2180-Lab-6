fbb4bb01548c357d10b958c88b5b44b931cb8912
Jhanelle Thomas <jhanelle.thomas02@mymona.uwi.edu>
2016-11-07 00:09:35 +0000

    get all definitions as xml

--- request.js
+++ request.js
@@ -1,16 +1,49 @@
 $(document).ready(function() {
     var searchButton = $("#search");
+    var searchAllButton = $("#searchAll");
     var result = $("#result");
     
     searchButton.on("click", function(element) {
         element.preventDefault();
         var word = $("input").val();
-        $.ajax('request.php?q=' + word, {
+        if (word != "") {
+            $.ajax("request.php?q=" + word, {
+                method: 'GET'
+            }).done(function(response) {
+                $(result).html(response);
+            }).fail(function() {
+                $(result).html("There was a problem with the request.");
+            });     
+        } else {
+            $(result).html("Please enter a word.");
+        }
+    });
+    
+    searchAllButton.on("click", function(element) {
+        element.preventDefault();
+        var list = document.createElement("ol");
+        $.ajax("request.php?q=&all=true", {
             method: 'GET'
         }).done(function(response) {
-            $("#result").html(response);
+            var definitions = $(response).find("item");
+            $(definitions).each(function() {
+                var listItem = document.createElement("li");
+                var heading = document.createElement("h3");
+                var para1 = document.createElement("p");
+                var para2 = document.createElement("p");
+                
+                $(heading).text($(this).find("word").text().toUpperCase());
+                $(para1).text($(this).find("definition").text());
+                $(para2).text("- " + $(this).find("author").text());
+                
+                $(listItem).append($(heading));
+                $(listItem).append($(para1));
+                $(listItem).append($(para2));
+                $(list).append($(listItem));
+            });
+            $(result).html($(list));
         }).fail(function() {
-            $("#result").html("There was a problem with the request.");
+            $(result).html("There was a problem with the request.");
         });
     });
 });
--- request.php
+++ request.php
@@ -4,15 +4,63 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if ($query != "") {
+    $definition = [
+        "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+        "bar" => "A place that sells alcholic beverages",
+        "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+        "html" => "The standard markup language for creating web pages and web applications.",
+        "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+        "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+        "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+    ];
+        
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+
+} else {
+
+    $xmldefinitions = '<?xml version="1.0" encoding="UTF-8"?>
+    <dictionary>
+        <item>
+            <word>definition</word>
+            <definition>A statement of the exact meaning of a word, especially in a dictionary.</definition>
+            <author>Merriam-Webster</author>
+        </item>
+        <item>
+            <word>bar</word>
+            <definition>A place that sells alcholic beverages</definition>
+            <author>Oxford</author>
+        </item>
+        <item>
+            <word>ajax</word>
+            <definition>Technique which involves the use of javascript and xml (or JSON)</definition>
+            <author>Microsoft</author>
+        </item>
+        <item>
+            <word>html</word>
+            <definition>The standard markup language for creating web pages and web applications.</definition>
+            <author>Tim Berners-Lee</author>
+        </item>
+        <item>
+            <word>css</word>
+            <definition>A style sheet language used for describing the presentation of a document written in a markup language."</definition>
+            <author>Håkon Wium Lie Bert Bos</author>
+        </item>
+        <item>
+            <word>javascript</word>
+            <definition>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</definition>
+            <author>Brendan Eich</author>
+        </item>
+        <item>
+            <word>php</word>
+            <definition>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</definition>
+            <author>Rasmus Lerdorf</author>
+        </item>
+    </dictionary>';
+    
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldefinitions);
+    echo $xmlOutput->asXML();
+}
